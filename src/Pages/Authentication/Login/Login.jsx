58619bf740d5b1a98caa4b295784caf77dc49bc3
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-05-27 22:45:07 +0530

    refactor

--- src/Pages/Authentication/Login/Login.jsx
+++ src/Pages/Authentication/Login/Login.jsx
@@ -1,16 +1,45 @@
+<<<<<<< Updated upstream
 import {MdEmail} from "react-icons/md";
 import {AiTwotoneLock} from "react-icons/ai";
 
+=======
+import { LoadingSpinner } from "../../../Components";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../../firebase/firebase-auth";
+import { toast } from "react-toastify";
+import { toastConstants } from "../../../constants/constants";
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { EmailIcon, LockIcon } from "../../../Components/Icons/Icons";
+>>>>>>> Stashed changes
 const Login = () => {
   return (
+<<<<<<< Updated upstream
    <form> 
    <div className="input-container mb-4">
    <MdEmail className="icon"/>
     <input type="email" name="email" placeholder="Enter Email"  className="input-field"/>
   </div>
+=======
+    <form onSubmit={formik.handleSubmit}>
+      <div className="input-container mb-4">
+        <EmailIcon className="icon" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          className="input-field"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+>>>>>>> Stashed changes
 
   <p className="text-red-600 mb-2"></p>
 
+<<<<<<< Updated upstream
   <div className="input-container">
   <AiTwotoneLock className="icon"/>
     <input
@@ -35,5 +64,38 @@ const Login = () => {
   </form>
   )
 }
+=======
+      <div className="input-container">
+        <LockIcon className="icon" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          className="input-field"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+      {formik.touched.password && formik.errors.password ? (
+        <p className="text-red-600 mb-2">{formik.errors.password}</p>
+      ) : null}
+      <button type="submit" className="btn">
+        {loading ? <LoadingSpinner> Logging in..</LoadingSpinner> : "Login"}
+      </button>
+      <span
+        className="btn bg-white text-blue-600 border-2 border-gray-300 cursor-pointer"
+        onClick={testUserLogin}
+      >
+        {testLoading ? (
+          <LoadingSpinner> Logging in..</LoadingSpinner>
+        ) : (
+          " Login with test credentials"
+        )}
+      </span>
+    </form>
+  );
+};
+>>>>>>> Stashed changes
 
 export default Login;
--- src/Pages/Authentication/Signup/Signup.jsx
+++ src/Pages/Authentication/Signup/Signup.jsx
@@ -1,45 +1,77 @@
+<<<<<<< Updated upstream
 import {BsPersonFill} from "react-icons/bs";
 import {MdEmail} from "react-icons/md";
 import {AiTwotoneLock} from "react-icons/ai";
+=======
+import { signUp } from "../../../firebase/firebase-auth";
+import { useFormik } from "formik";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { toastConstants } from "../../../constants/constants";
+import {
+  EmailIcon,
+  LockIcon,
+  PersonIcon,
+} from "../../../Components/Icons/Icons";
+>>>>>>> Stashed changes
 
 const Signup = () => {
   return (
-    <form>
-    <div className="input-container mb-4">
-    <BsPersonFill className="icon"/>
-    <input
-      type="text"
-      name="name"
-      id=""
-      placeholder="Enter Name"
-      className="input-field"
-    />
-  </div>
-  <div className="input-container mb-4">
-    <MdEmail className="icon"/>
-    <input
-      type="email"
-      name="email"
-      placeholder="Enter Email"
-      className="input-field"
-    />
-  </div>
-  
-  <div className="input-container">
-    <AiTwotoneLock className="icon" />
-    <input
-      type="password"
-      name="password"
-      placeholder="Enter Password" 
-      className="input-field"
-    />
-  </div>
-  
-  <button  type="submit" className="btn">
-         Signup
-  </button>
-  </form>
-  )
-}
+<<<<<<< Updated upstream
+
+=======
+    <form onSubmit={formik.handleSubmit}>
+      <div className="input-container mb-4">
+        <PersonIcon className="icon" />
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          className="input-field"
+          onChange={formik.handleChange}
+          value={formik.values.name}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+      {formik.touched.name && formik.errors.name ? (
+        <p className="mb-2 text-red-600">{formik.errors.name}</p>
+      ) : null}
+      <div className="input-container mb-4">
+        <EmailIcon className="icon" />
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          className="input-field"
+          onChange={formik.handleChange}
+          value={formik.values.email}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+      {formik.touched.email && formik.errors.email ? (
+        <p className="mb-2 text-red-600">{formik.errors.email}</p>
+      ) : null}
+      <div className="input-container">
+        <LockIcon className="icon" />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          className="input-field"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+          onBlur={formik.handleBlur}
+        />
+      </div>
+      {formik.touched.password && formik.errors.password ? (
+        <p className="text-red-600 mb-2">{formik.errors.password}</p>
+      ) : null}
+      <button type="submit" className="btn">
+        {loading ? "Signing in..." : "Signup"}
+      </button>
+    </form>
+  );
+};
+>>>>>>> Stashed changes
 
 export default Signup;
